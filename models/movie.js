00a54d4df640029fa182be9c224ba4d066e36a2a
tsooli97 <tsooli.it@gmail.com>
2024-09-07 12:01:55 +0300

    Updated "movie" model constraints

--- models/movie.js
+++ models/movie.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-  name: { type: String, required: true, minLength: 1, maxLength: 50 },
+  name: { type: String, required: true, minLength: 2, maxLength: 100 },
   year: { type: Number, required: true, min: 1800, max: 2030 },
   bio: { type: String, maxLength: 500 },
-  director: { type: String, maxLength: 100 },
+  director: { type: String, minLength: 2, maxLength: 100 },
   starring: [
     {
-      actor: { type: String, required: true },
-      role: { type: String, required: true },
+      actor: { type: String, minLength: 2, maxLength: 100, required: true },
+      role: { type: String, minLength: 1, maxLength: 100, required: true },
     },
   ],
-  genre: [{ type: String }],
+  genre: [{ type: String, minLength: 2, maxLength: 100 }],
 });
 
 module.exports = mongoose.model("Movie", MovieSchema, "myMovies");
